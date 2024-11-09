913abcf797d9ce4eb658073847e3924871e0d23e
levinyt <madlevinho@gmail.com>
2024-11-09 14:31:50 +0530

    v2

--- src/home.jsx
+++ src/home.jsx
@@ -4,24 +4,15 @@ const Home=()=>{
         <div className="home">
             <h2>Home Page</h2>
 
-            <Exampletwo Name="Levin" Age="21" Job="Full stack web developer"/>
-            <Exampletwo Name="yuva" Age="21" Job="Full stack web developer"/>
+            <Exampletwo Name="Levin" Age="21" Job="Full stack web developer" Exp="2 years"/>
+            <Exampletwo Name="yuva" Age="21" Job="Full stack web developer" Exp="3 years"/>
+
 
         </div>
 
     )
 }
-// const Example=()=>{
-//     return(
-//         <div>
-//             <div>
-//                 <h3>Name:levin</h3>
-//                 <h3>Age:20</h3>
-//                 <h3>Job:Fullstack Web development</h3>
-//             </div>
-//         </div>
-//     )
-// }
+
 
 const Exampletwo=(props)=>{
     return(
@@ -29,8 +20,12 @@ const Exampletwo=(props)=>{
         <h3>Name:{props.Name}</h3><br />
         <h3>Age:{props.Age}</h3><br />
         <h3>Job:{props.Job}</h3>
+        <h3>Exp:{props.Exp}</h3>
     </div>
     )
 }
 
 export default Home
+
+
+
